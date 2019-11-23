d3e458f6a62b51d50ffc249b3edf3fb8459b6bce
sooki <jennieee267@gmail.com>
2019-11-23 18:29:02 +0900

    초기셋팅

---
+++ src/layout/AuthLayout.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="main-content bg-default" style="background-color: #d9e4d9 !important;">
+        <!-- Navbar -->
+        <base-nav class="navbar-top navbar-horizontal navbar-dark"
+                  containerClasses="px-4 container"
+                  expand>
+            <template>
+                <!-- Collapse header -->
+                <div class="navbar-collapse-header d-md-none">
+                    <div class="row">
+                        <div class="col-6 collapse-brand">
+                            <router-link to="/">
+                                <img src="img/brand/white.png">
+                            </router-link>
+                        </div>
+                        <div class="col-6 collapse-close">
+                            <button type="button"
+                                    @click="closeMenu"
+                                    class="navbar-toggler"
+                                    aria-label="Toggle sidenav">
+                                <span></span>
+                                <span></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <!-- Navbar items -->
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <router-link slot="brand" class="navbar-brand" to="/">
+                            <img src="img/brand/white.png"/>
+                        </router-link>
+                    </li>
+                </ul>
+            </template>
+        </base-nav>
+        <!-- Header -->
+        <div class="header bg-gradient-success py-7 py-lg-8">
+
+        </div>
+        <!-- Page content -->
+        <div class="container mt--8 pb-5">
+            <slide-y-up-transition mode="out-in" origin="center top">
+                <router-view></router-view>
+            </slide-y-up-transition>
+        </div>
+        <footer class="py-5">
+            <div class="container">
+                <div class="row align-items-center justify-content-xl-between">
+                    <div class="col-xl-6">
+                        <div class="copyright text-center text-xl-left text-muted">
+                            &copy; {{year}} <a href="#" class="font-weight-bold ml-1"
+                                               target="_blank">CACTUS</a>
+                        </div>
+                    </div>
+                    <div class="col-xl-6">
+                        <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+                            <li class="nav-item">
+                                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">이지수</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">김정우</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">이근환</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md"
+                                   class="nav-link" target="_blank">김남현</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md"
+                                   class="nav-link" target="_blank">김은아</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+<script>
+  import { SlideYUpTransition } from 'vue2-transitions'
+
+  export default {
+    name: 'auth-layout',
+    components: {
+      SlideYUpTransition
+    },
+    data() {
+      return {
+        year: new Date().getFullYear(),
+        showMenu: false
+      }
+    }
+  }
+</script>
+<style>
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,70 @@
+<template>
+
+        <div class="row justify-content-end" style="min-height: 600px; background-image: url(../../public/img/backgroud/b1.jpg); background-size: cover; /* background-position: center top; */">
+            <div class="col-lg-5 col-md-7" style="margin-top: 7%;">
+                <div class="card bg-secondary shadow border-0" style="background-color: #e8ebeccc !important;">
+                    <div class="card-body px-lg-5 py-lg-5">
+                        <div class="collapse-brand" style="text-align: center;padding: 10px;">
+                            <router-link to="/">
+                                <img src="img/brand/white.png" style="width:80%;">
+                            </router-link>
+                        </div>
+                            <base-input class="input-group-alternative mb-3"
+                                        placeholder="아이디"
+                                        addon-left-icon="ni ni-email-83"
+                                        v-model="model.email">
+                            </base-input>
+
+                            <base-input class="input-group-alternative"
+                                        placeholder="비밀번호"
+                                        type="password"
+                                        addon-left-icon="ni ni-lock-circle-open"
+                                        v-model="model.password">
+                            </base-input>
+
+                            <base-checkbox class="custom-control-alternative">
+                                <span class="text-muted">자동로그인</span>
+                            </base-checkbox>
+                            <div class="text-center">
+                                <base-button type="primary" class="my-4" @click="doLogin">로그인</base-button>
+                            </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-6">
+                        <a href="#" class="text-light"><small>비밀번호를 잊어버리셨나요?</small></a>
+                    </div>
+                    <div class="col-6 text-right">
+                        <router-link to="/register" class="text-light"><small>회원가입</small></router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+<script>
+  export default {
+    name: 'login',
+    data() {
+      return {
+        model: {
+          email: '',
+          password: ''
+        }
+      }
+    },
+      methods: {
+          doLogin: function(){
+              this.$http.post('/api/user', {
+                  id: 1234
+                  ,e_mail: this.email
+                  ,name: this.pw
+              })
+                  .then((result) => {
+                      console.log(result.data)
+                  })
+          }
+      }
+  }
+</script>
+<style>
+</style>
