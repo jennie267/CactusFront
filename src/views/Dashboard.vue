55362ace05fc3b18ad535188aa5d6dc0ddeadd24
gunan <power4889@gmail.com>
2019-12-02 21:12:47 +0900

    comm

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,22 +4,22 @@
         </base-header>
         <div class="row">
             <div class="col mx-5">
-                    <div class="row my-3">
-                        <FullCalendar
-                                defaultView="dayGridMonth"
-                                ref="fullCalendar"
-                                :plugins="calendarPlugins"
-                                locale="ko"
-                                :weekends="true"
-                                :header="{
-                                        left: 'prev',
-                                        center: 'title',
-                                        right: 'next'
-                                }"
-                                :selectable="true"
-                                @dateClick="dateClick"
-                        />
-                    </div>
+                <div class="row my-3">
+                    <FullCalendar
+                            defaultView="dayGridMonth"
+                            ref="fullCalendar"
+                            :plugins="calendarPlugins"
+                            locale="ko"
+                            :weekends="true"
+                            :header="{
+                                    left: 'prev',
+                                    center: 'title',
+                                    right: 'next'
+                            }"
+                            :selectable="true"
+                            @dateClick="dateClick"
+                    />
+                </div>
                 <div class="row my-3">
                     <div class="card shadow" style="width:100%;">
                         <div class="card-header bg-transparent">
@@ -31,8 +31,8 @@
                             <div class="row">
                                 <vue-good-table
                                         style="width:100%;"
-                                        :columns="columns"
-                                        :rows="rows"
+                                        :columns="childHeader"
+                                        :rows="childData"
                                         @on-row-click="onRowClick"/>
                             </div>
                         </div>
@@ -41,9 +41,24 @@
             </div>
             <div class="col mx-5">
                 <div class="row my-3">
-                    <dailySchedule-table :nameOfChild="date" title="Light Table"></dailySchedule-table>
+                    <div class="card shadow" style="width:100%;">
+                        <div class="card-header bg-transparent">
+                            <div class="row">
+                                <h3 class="mb-0">{{date}} 일정</h3>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <vue-good-table
+                                        style="width:100%;"
+                                        :columns="columns"
+                                        :rows="rows"/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="row my-3">
+                    <dashboard-profile :user="selectedUser" style="width:100%; margin-top:80px;"></dashboard-profile>
                 </div>
             </div>
         </div>
@@ -53,15 +68,15 @@
 import FullCalendar from '@fullcalendar/vue'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import DailyScheduleTable from './Tables/DailyScheduleTable'
 import 'vue-good-table/dist/vue-good-table.css'
 import { VueGoodTable } from 'vue-good-table/src';
+import DashboardProfile from './Dashboard/DashboardProfile'
 
   export default {
     components: {
         FullCalendar,
-        DailyScheduleTable,
         VueGoodTable,
+        DashboardProfile,
     },
     data() {
       return {
@@ -70,7 +85,7 @@ import { VueGoodTable } from 'vue-good-table/src';
               , interactionPlugin
           ],
           date: new Date().getFullYear().toString()+'-'+(new Date().getMonth()+1).toString()+'-'+new Date().getDate().toString(),
-          columns: [
+          childHeader: [
               {
                   field: 'userId',
                   hidden: true,
@@ -95,30 +110,59 @@ import { VueGoodTable } from 'vue-good-table/src';
                   dateOutputFormat: 'yyyy-MM-dd',
                   width: '25%',
               },
+          ],
+          childData: [],
+          selectedUser: {},
+          periodHeader: [
               {
-                  label: '정보',
-                  field: 'info',
-                  width: '5%',
+                  field: 'userId',
+                  hidden: true,
+              },
+              {
+                  label: '프로필',
+                  field: 'profile',
+                  width: '8%',
                   tdClass: 'text-center',
                   html: true,
               },
+              {
+                  label: '이름',
+                  field: 'name',
+                  width: '25%',
+              },
+              {
+                  label: '생일',
+                  field: 'birthday',
+                  type: 'date',
+                  dateInputFormat: 'yyyyMMdd',
+                  dateOutputFormat: 'yyyy-MM-dd',
+                  width: '25%',
+              },
           ],
-          rows: [],
+          periodData: [],
       }
     },
     methods: {
         dateClick(info) {
-            this.date = info.dateStr
+            this.date = info.dateStr;
+            console.log(`/api/schedule/day/user/${this.user.userId}/${this.date.replace(/-/gi,'')}`)
+            this.$http.get(`/api/schedule/day/user/${this.user.userId}/${this.date.replace(/-/gi,'')}`,  { headers: { Authorization: `Bearer ${this.user.token}` } })
+                .then(res => {
+                    console.log(res.data)
+                    this.periodData = res.data;
+                });
         },
+        onRowClick(select) {
+            this.selectedUser = select.row
+        }
     },
       mounted() {
           this.$http.get(`/api/user/children/${this.user.userId}`,  { headers: { Authorization: `Bearer ${this.user.token}` } })
               .then(res => {
-                  this.rows = res.data.users;
-                  this.rows.forEach(user => user.info = '<a class="avatar avatar-sm rounded-circle" style="cursor: pointer;"></a>');
-                  this.rows.forEach(user => user.profile = '<a href="#" class="avatar avatar-sm rounded-circle">\n' +
-                      '              <img alt="Image placeholder" src="'+user.profileUrl+'" style="width:90%;">\n' +
-                      '            </a>');
+                  this.childData = res.data.users;
+                  this.childData.forEach(user => user.profile = '<a class="avatar avatar-sm rounded-circle" style="cursor: pointer;">\n' +
+                      '<img alt="Image placeholder" src="'+user.profileUrl+'" style="width:90%;"/>\n' +
+                      '</a>');
               });
       }
   };
---
+++ src/views/Dashboard/DashboardProfile.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <base-header type="gradient-success" class="pb-6 pb-8">
+        </base-header>
+        <div class="container-fluid mt--7">
+            <div class="row">
+                <div class="col-xl-8 order-xl-1">
+                    <div class="card card-profile shadow">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-3 order-lg-2">
+                                <div class="card-profile-image">
+                                    <a>
+                                        <img v-if="typeof user.profileUrl=='undefined' || user.profileUrl===''" src="img/theme/typeUser.jpg" class="rounded-circle">
+                                        <img v-else :src="user.profileUrl" class="rounded-circle">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
+                            <div class="d-flex justify-content-between">
+                            </div>
+                        </div>
+                        <div class="card-body pt-0 pt-md-4 mt-5">
+                            <div class="text-center">
+                                <h1>{{user.name}}</h1>
+                                <h2>{{user.tel}}</h2>
+                                <h2>{{user.addrMain}} {{user.addrSub}}</h2>
+                                <h2>{{user.email}}</h2>
+                                <h2>{{user.birthday}}</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        },
+        props: {
+            user: Object
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
